47afccefb9f683f13c4789e773d0b4d3fff32c30
legymen <80207339+legymen@users.noreply.github.com>
2025-02-07 16:33:12 +0100

    Snyggat till lite, och förenklat
    Tagit bort derivatafiltreringen helt. Kör bara med Servo.write()

--- Testrigg_PID/Testrigg_PID.ino
+++ Testrigg_PID/Testrigg_PID.ino
@@ -1,53 +1,58 @@
 #include <Wire.h>
 #include <Servo.h>
-// 16:23 2025-01-29.
+
+// Set-constants
 const float BIAS_POWER = 60;
 const float DESIRED_ANGLE = 0;
-const float MAX_MOTOR_POWER = 90;
+const float MAX_MOTOR_POWER = 80; // NOTE! FOR SAFETY!
+const float LOOP_DELAY_TIME_MS // Loop delay time in millisecs
 
+// Pins
 const int POT_PIN_KP = A0;
 const int POT_PIN_KI = A1;
 const int POT_PIN_KD = A2;
 const int ESC_PWM_PIN = 11;
-const int ZERO_BTN_PIN = 5;
 
+// PID-constants
 float Kp = 1;
 float Ki = 1;
 float Kd = 1;
 
-const float D_ALPHA = 0.15;
-
-Servo ESC;
+// Global variables
+Servo BLDC_motor; //Object for the bldc-motor with its ESC
 
 float roll;
 float pitch;
-float measuredAngle, error, previousError, deltaError, sumError, pidPower, D, dt;
+float measuredAngle, error, previousError, derivativeError, sumError, pidPower, P, I, D, dt;
 float previousDerivative = 0;
-float now;
-float lasttime;
+float now, lasttime;
 
 void setup() {
-  sumError = 0;
-  pinMode(ZERO_BTN_PIN, INPUT_PULLUP);
-
+  
+  // Calibrating the gyro
   Serial.begin(921600);
-  Serial.println("Initializing ...");
-  startgyro(); // Kalibrerar även gyron.
+  Serial.println("Initializing gyro ...");
+  startgyro();
 
-  ESC.attach(ESC_PWM_PIN, 1000, 2000);
-  ESC.write(0);
+  // Attaching and zeroing the bldc
+  BLDC_motor.attach(ESC_PWM_PIN, 1000, 2000);
+  BLDC_motor.write(0);
   Serial.println("Zeroing BLDC ... starting in 4 sec");
   delay(4000);
-
   Serial.println("Lets go!");
+  
   lasttime = micros();
+  sumError = 0;
 }
 
 void loop() {
+  
+  // Calculating the loop-time dt
   now = micros();
   dt = (now - lasttime) / 1000000;
   lasttime = now;
 
+  // Read PID-constants from the potentiometers
   Kp = analogRead(POT_PIN_KP); 
   Kp /= (1023 / 80);
 
@@ -57,49 +62,50 @@ void loop() {
   Kd = analogRead(POT_PIN_KD);
   Kd /= (1023 / 20);
 
-  if (digitalRead(ZERO_BTN_PIN) == HIGH) {
-    sumError = 0;
-  }
-
+  // Read the current angle from the accelerometer sensor
   getangles(pitch, roll);
   measuredAngle = (-1) * roll;
 
+  // Calculate error, error integral and error derivative
   error = DESIRED_ANGLE - measuredAngle;
-  sumError += error * dt;
+  sumError = sumError + error * dt;
   sumError = constrain(sumError, -40000, 40000);
-
-  float rawDerivative = (error - previousError) / dt;
-  float P = Kp * error;
-  float I = Ki * sumError;
-  D = Kd * ((D_ALPHA * previousDerivative) + ((1 - D_ALPHA) * rawDerivative));
-
+  derivativeError = (error - previousError) / dt;
   previousError = error;
-  previousDerivative = rawDerivative;
-
+  
+  // Calculate the PID-power
+  P = Kp * error;
+  I = Ki * sumError;
+  D = Kd * derivativeError;
   pidPower = P + I + D;
   pidPower = constrain(pidPower, -3000, 3000);
-  // Ta kanske bort BIAS_POWER från mapen?
-  pidPower = map(pidPower, -3000, 3000, -BIAS_POWER, 180 - BIAS_POWER) + BIAS_POWER;
-  pidPower = constrain(pidPower, 0, MAX_MOTOR_POWER);
-  ESC.write(pidPower); 
 
+  // Map PID-power for the motor, add bias power and run the motor
+  pidPower = map(pidPower, -3000, 3000, -180, 180) + BIAS_POWER;
+  pidPower = constrain(pidPower, 0, MAX_MOTOR_POWER); // NOTE! FOR SAFETY!
+  BLDC_motor.write(pidPower); 
+
+  // Display info in the computers console
   schreibeinformation();
-  delay(30);
+  
+  // Loop-delay for stability
+  delay(LOOP_DELAY_TIME_MS);
 }
 
 void schreibeinformation()
 {
+  Serial.print("Kp=");
   Serial.print(Kp, 2);
-  Serial.print(", ");
+  Serial.print(", Ki=");
   Serial.print(Ki, 2);
-  Serial.print(", ");
+  Serial.print(", Kd =");
   Serial.print(Kd, 2);
-  Serial.print(", ");
+  Serial.print(", error=");
   Serial.print(error, 1);
-  Serial.print(", ");
+  Serial.print(", sumError=");
   Serial.print(sumError, 1);
-  Serial.print(", ");
+  Serial.print(", dt=");
   Serial.print(dt, 6);
-  Serial.print(", ");
+  Serial.print(", pidPower=");
   Serial.println(pidPower);
 }
